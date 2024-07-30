73e57fc256702816d0d2147bb3d8a401fdca8f9b
yarogono <naocya@naver.com>
2024-07-30 13:50:46 +0900

    테스트 코드 수정

--- DataStructure.Test/MyLinkedListTest.cs
+++ DataStructure.Test/MyLinkedListTest.cs
@@ -5,18 +5,18 @@ namespace DataStructure.Test
 {
     public class MyLinkedListTest
     {
-        LinkedList<int> linkedList;
-        MyLinkedList<int> myList;
+        LinkedList<string> linkedList;
+        MyLinkedList<string> myList;
 
         [Fact]
         public void linkedList_생성()
         {
-            linkedList = new LinkedList<int>();
-            linkedList.Append(1);
-            linkedList.Append(2);
-            linkedList.Append(3);
+            linkedList = new LinkedList<string>();
+            linkedList.Append("테스트1");
+            linkedList.Append("테스트2");
+            linkedList.Append("테스트3");
 
-            myList = new MyLinkedList<int>();
+            myList = new MyLinkedList<string>();
             myList.AddAll(linkedList);
         }
 
@@ -29,121 +29,107 @@ namespace DataStructure.Test
         [Fact]
         public void linkedList_맨앞에삽입()
         {
-            ////given 빈 연결리스트 생성 후 김수현 add
-            //ll = new LinkedList<String>();
-            //ll.add("김수현");
+            //given 빈 연결리스트 생성 후 김수현 add
+            myList.AddFirst("김수현");
 
-            ////when 연결리스트 가장 처음에 전지현 add
-            //ll.addFirst("전지현");
+            // when 연결리스트 가장 처음에 전지현 add
+            myList.AddFirst("전지현");
 
-            ////then 연결리스트에 전지현, 김수현 순서로 들어간다.
-            //assertEquals("전지현", ll.get(0));
-            //assertEquals("김수현", ll.get(1));
-
-            //System.out.println("linkedList_맨앞에삽입 : " + ll.toString());
+            //then 연결리스트에 전지현, 김수현 순서로 들어간다.
+            Assert.Equal("전지현", myList.Find("전지현").Value);
+            Assert.Equal("김수현", myList.Find("김수현").Value);
         }
 
         [Fact]
         public void linkedList_맨끝에삽입()
         {
-            ////given 빈 연결리스트 생성 후 김수현 add
-            //ll = new LinkedList<String>();
-            //ll.add("김수현");
+            //given 빈 연결리스트 생성 후 김수현 add
+            myList.AddLast("김수현");
 
-            ////when 연결리스트 가장 끝에 전지현 add
-            //ll.addLast("전지현");
+            //when 연결리스트 가장 끝에 전지현 add
+            myList.AddLast("전지현");
 
-            ////then 연결리스트에 김수현, 전지현 순서로 들어간다.
-            //assertEquals("김수현", ll.get(0));
-            //assertEquals("전지현", ll.get(1));
-
-            //System.out.println("linkedList_맨끝에삽입 : " + ll.toString());
+            //then 연결리스트에 김수현, 전지현 순서로 들어간다.
+            Assert.Equal("김수현", myList.Find("김수현").Value);
+            Assert.Equal("전지현", myList.Find("전지현").Value);
         }
 
 
         [Fact]
         public void linkedList_중간에삽입()
         {
-            ////given 빈 연결리스트 생성 후 김수현, 박해진 add
-            //ll = new LinkedList<String>();
-            //ll.add("김수현");
-            //ll.add("박해진");
+            //given 빈 연결리스트 생성 후 김수현, 박해진 add
+            myList.AddFirst("김수현");
+            myList.AddFirst("박해진");
 
-            ////when 1번 index에 전지현 add
-            //ll.add(1, "전지현");
+            var myListNode = myList.Find("김수현");
 
-            ////then 연결리스트에 김수현, 전지현 순서로 들어간다.
-            //assertEquals("김수현", ll.get(0));
-            //assertEquals("전지현", ll.get(1));
-            //assertEquals("박해진", ll.get(2));
+            //when 1번 index에 전지현 add
+            myList.AddBefore(myListNode, new LinkedListNode<string>("전지현"));
 
-            //System.out.println("linkedList_중간에삽입 : " + ll.toString());
+            //then 연결리스트에 김수현, 전지현 순서로 들어간다.
+            Assert.Equal("김수현", myList.Find("김수현").Value);
+            Assert.Equal("전지현", myList.Find("전지현").Value);
+            Assert.Equal("박해진", myList.Find("박해진").Value);
         }
 
         [Fact]
         public void linkedList_빈노드삭제()
         {
-            ////given 빈 연결리스트 생성
-            //ll = new LinkedList<String>();
+            //given 빈 연결리스트 생성
+            myList.Clear();
 
-            ////when 첫번째 노드 삭제
-            //ll.removeFirst();
+            //when 첫번째 노드 삭제
+            myList.RemoveFirst();
 
-            ////then 삭제되지 않는다. (에러뜸)
-            //System.out.println("linkedList_빈노드삭제 : " + ll.toString());
+            // ToDo : 에러 출력 확인
         }
 
         [Fact]
         public void linkedList_첫노드삭제()
         {
-            ////given 빈 연결리스트 생성 후 김수현, 전지현 add
-            //ll = new LinkedList<String>();
-            //ll.add("김수현");
-            //ll.add("전지현");
+            //given 빈 연결리스트 생성 후 김수현, 전지현 add
+            myList.AddFirst("김수현");
+            myList.AddFirst("전지현");
 
-            ////when 첫번째 노드 삭제
-            //ll.removeFirst();
+            //when 첫번째 노드 삭제
+            myList.RemoveFirst();
 
-            ////then 연결리스트에 전지현만 남는다.
-            //assertEquals("전지현", ll.get(0));
-
-            //System.out.println("linkedList_첫노드삭제 : " + ll.toString());
+            //then 연결리스트에 전지현만 남는다.
+            Assert.Equal("전지현", myList.Find("전지현").Value);
+            Assert.Equal(null, myList.Find("김수현"));
         }
 
         [Fact]
         public void linkedList_마지막노드삭제()
         {
-            ////given 빈 연결리스트 생성 후 김수현, 전지현 add
-            //ll = new LinkedList<String>();
-            //ll.add("김수현");
-            //ll.add("전지현");
+            //given 빈 연결리스트 생성 후 김수현, 전지현 add
+            myList.AddFirst("김수현");
+            myList.AddFirst("전지현");
 
-            ////when 마지막 노드 삭제
-            //ll.removeLast();
+            //when 마지막 노드 삭제
+            myList.RemoveLast();
 
-            ////then 연결리스트에 김수현만 남는다.
-            //assertEquals("김수현", ll.get(0));
-
-            //System.out.println("linkedList_마지막노드삭제 : " + ll.toString());
+            //then 연결리스트에 김수현만 남는다.
+            Assert.Equal("김수현", myList.Find("김수현").Value);
+            Assert.Equal(null, myList.Find("전지현"));
         }
 
         [Fact]
         public void linkedList_중간노드삭제()
         {
-            ////given 빈 연결리스트 생성 후 김수현, 전지현, 박해진 add
-            //ll = new LinkedList<String>();
-            //ll.add("김수현");
-            //ll.add("전지현");
-            //ll.add("박해진");
+            //given 빈 연결리스트 생성 후 김수현, 전지현, 박해진 add
+            myList.AddFirst("김수현");
+            myList.AddFirst("전지현");
+            myList.AddFirst("박해진");
 
-            ////when 전지현 삭제
-            //ll.remove("전지현");
+            //when 전지현 삭제
+            myList.Remove("전지현");
 
-            ////then 연결리스트에 김수현, 박해진 순서로 남는다.
-            //assertEquals("김수현", ll.get(0));
-            //assertEquals("박해진", ll.get(1));
-
-            //System.out.println("linkedList_중간노드삭제 : " + ll.toString());
+            //then 연결리스트에 김수현, 박해진 순서로 남는다.
+            Assert.Equal("김수현", myList.Find("김수현").Value);
+            Assert.Equal("박해진", myList.Find("박해진").Value);
+            Assert.Equal(null, myList.Find("전지현"));
         }
     }
 }
