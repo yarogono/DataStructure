047eef3ec47a910c327d3566e09b64f0d3f7fb7a
yarogono <70641418+yarogono@users.noreply.github.com>
2024-08-19 05:46:29 +0900

    WiKiPhilosophy 과제 시작

---
+++ DataStructure/WikiPhilosophy.cs
@@ -0,0 +1,18 @@
+﻿namespace DataStructure
+{
+    public class WikiPhilosophy
+    {
+
+        public static void Main(string[] args)
+        {
+            string destination = "https://en.wikipedia.org/wiki/Philosophy";
+            string source = "https://en.wikipedia.org/wiki/Java_(programming_language)";
+
+            testConjecture(destination, source, 10);
+        }
+
+        public static void testConjecture(string destination, string source, int limit)
+        {
+        }
+    }
+}
