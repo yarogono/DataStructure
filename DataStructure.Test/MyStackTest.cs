1e7678141a53f8fb343ac2316c9fe882a092c4e8
yarogono <naocya@naver.com>
2024-08-28 13:23:40 +0900

    MyStackTest 테스트 코드 로직 작성

--- DataStructure.Test/MyStackTest.cs
+++ DataStructure.Test/MyStackTest.cs
@@ -22,13 +22,20 @@ namespace DataStructure.Test
         public void MyStackPushTest()
         {
             // Given
-
+            string myStack1 = "MyStack1";
+            string myStack2 = "MyStack2";
+            string myStack3 = "MyStack3";
 
             // When
+            myStackString.Push(myStack1);
+            myStackString.Push(myStack2);
+            myStackString.Push(myStack3);
 
 
             // Then
-
+            Assert.True(myStackString.Contains(myStack1));
+            Assert.Equal(3, myStackString.Count());
+            Assert.Equal(myStack3, myStackString.Peek());
         }
 
 
@@ -36,40 +43,38 @@ namespace DataStructure.Test
         public void MyStackPopTest()
         {
             // Given
-
+            string myStack1 = "MyStack1";
+            string myStack2 = "MyStack2";
+            string myStack3 = "MyStack3";
+            myStackString.Push(myStack1);
+            myStackString.Push(myStack2);
+            myStackString.Push(myStack3);
 
             // When
-
+            string popMyStack3 = myStackString.Pop();
 
             // Then
-
+            Assert.Equal(myStack3, popMyStack3);
+            Assert.Equal(2, myStackString.Count());
         }
 
         [Fact]
         public void MyStackClearTest()
         {
             // Given
-
-
-            // When
-
-
-            // Then
-
-        }
-
-
-        [Fact]
-        public void MyStackCountTest()
-        {
-            // Given
-
+            string myStack1 = "MyStack1";
+            string myStack2 = "MyStack2";
+            string myStack3 = "MyStack3";
+            myStackString.Push(myStack1);
+            myStackString.Push(myStack2);
+            myStackString.Push(myStack3);
 
             // When
-
+            myStackString.Clear();
 
             // Then
-
+            Assert.Equal(0, myStackString.Count());
+            Assert.False(myStackString.Contains(myStack3));
         }
 
 
@@ -77,13 +82,18 @@ namespace DataStructure.Test
         public void MyStackToArrayTest()
         {
             // Given
-
+            string myStack1 = "MyStack1";
+            string myStack2 = "MyStack2";
+            string myStack3 = "MyStack3";
+            myStackString.Push(myStack1);
+            myStackString.Push(myStack2);
+            myStackString.Push(myStack3);
 
             // When
-
+            string[] myStackArr = myStackString.ToArray();
 
             // Then
-
+            Assert.Equal(myStackArr.Length, myStackString.Count());
         }
 
 
@@ -91,13 +101,25 @@ namespace DataStructure.Test
         public void MyStackContainsTest()
         {
             // Given
+            string myStack1 = "MyStack1";
+            string myStack2 = "MyStack2";
+            string myStack3 = "MyStack3";
+            myStackString.Push(myStack1);
+            myStackString.Push(myStack2);
+            myStackString.Push(myStack3);
 
+            bool[] isContainsArr = new bool[3];
 
             // When
-
+            isContainsArr[0] = myStackString.Contains(myStack1);
+            isContainsArr[1] = myStackString.Contains(myStack2);
+            isContainsArr[3] = myStackString.Contains(myStack3);
 
             // Then
-
+            foreach (bool item in isContainsArr)
+            {
+                Assert.True(item);
+            }
         }
     }
 }
