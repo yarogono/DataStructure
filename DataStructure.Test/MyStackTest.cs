b010ad8aa48e91390057b2f6347164bb88e4d8f4
yarogono <naocya@naver.com>
2024-08-28 09:53:40 +0900

    MyStack 테스트 코드 함수 정의

---
+++ DataStructure.Test/MyStackTest.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure.Test
+{
+    public class MyStackTest
+    {
+        private MyStack<string> myStackString;
+        private MyStack<int> myStackNumber;
+
+
+        public MyStackTest()
+        {
+            this.myStackString = new();
+            this.myStackNumber = new();
+        }
+
+        [Fact]
+        public void MyStackPushTest()
+        {
+            // Given
+
+
+            // When
+
+
+            // Then
+
+        }
+
+
+        [Fact]
+        public void MyStackPopTest()
+        {
+            // Given
+
+
+            // When
+
+
+            // Then
+
+        }
+
+        [Fact]
+        public void MyStackClearTest()
+        {
+            // Given
+
+
+            // When
+
+
+            // Then
+
+        }
+
+
+        [Fact]
+        public void MyStackCountTest()
+        {
+            // Given
+
+
+            // When
+
+
+            // Then
+
+        }
+
+
+        [Fact]
+        public void MyStackToArrayTest()
+        {
+            // Given
+
+
+            // When
+
+
+            // Then
+
+        }
+
+
+        [Fact]
+        public void MyStackContainsTest()
+        {
+            // Given
+
+
+            // When
+
+
+            // Then
+
+        }
+    }
+}
--- DataStructure/MyStack.cs
+++ DataStructure/MyStack.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Diagnostics;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 
 namespace DataStructure
 {
