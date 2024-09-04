a95d8237d864db12a03b821b970ad33856bda959
yarogono <naocya@naver.com>
2024-09-04 13:55:38 +0900

    MyQueue 클래스 생성

---
+++ DataStructure/MyQueue.cs
@@ -0,0 +1,13 @@
+namespace DataStructure
+{
+    public class MyQueue
+    {
+        private int _size;
+
+        public int Count { get { return _size; } }
+
+        public MyQueue()
+        {
+        }
+    }
+}
