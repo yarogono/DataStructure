bac263b34d6bbaf3173fbd2a36bf7cac2dc958a7
yarogono <naocya@naver.com>
2024-09-06 13:28:18 +0900

    MyQueue 테스트 코드 추가

--- DataStructure.Test/MyQueueTest.cs
+++ DataStructure.Test/MyQueueTest.cs
@@ -2,7 +2,143 @@
 {
     public class MyQueueTest
     {
+        private MyQueue _myQueue;
 
-        
+        public MyQueueTest()
+        {
+        }
+
+        [Fact]
+        public void Count_test()
+        {
+            Assert.Fail("ToDo");
+            // Given
+
+
+            // When
+
+
+            // Then
+
+        }
+
+
+        [Fact]
+        public void Clear_test()
+        {
+            Assert.Fail("ToDo");
+            // Given
+
+
+            // When
+
+
+            // Then
+
+        }
+
+
+        [Fact]
+        public void Contains_test()
+        {
+            Assert.Fail("ToDo");
+            // Given
+
+
+            // When
+
+
+            // Then
+
+        }
+
+        [Fact]
+        public void Enqueue_test()
+        {
+            Assert.Fail("ToDo");
+            // Given
+
+
+            // When
+
+
+            // Then
+
+        }
+
+
+        [Fact]
+        public void Dequeue_test()
+        {
+            Assert.Fail("ToDo");
+            // Given
+
+
+            // When
+
+
+            // Then
+
+        }
+
+
+        [Fact]
+        public void Peek_test()
+        {
+            Assert.Fail("ToDo");
+            // Given
+
+
+            // When
+
+
+            // Then
+
+        }
+
+
+        [Fact]
+        public void ToArray_test()
+        {
+            Assert.Fail("ToDo");
+            // Given
+
+
+            // When
+
+
+            // Then
+
+        }
+
+        [Fact]
+        public void TryPeek_test()
+        {
+            Assert.Fail("ToDo");
+
+            // Given
+
+
+            // When
+
+
+            // Then
+
+        }
+
+        [Fact]
+        public void TryDequeue_test()
+        {
+            Assert.Fail("ToDo");
+
+            // Given
+
+
+            // When
+
+
+            // Then
+
+        }
     }
 }
