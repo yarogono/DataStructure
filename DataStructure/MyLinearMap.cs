0a51a8e7f0d52ec4145e52ad1b7bb211644431de
yarogono <70641418+yarogono@users.noreply.github.com>
2024-09-15 10:12:11 +0900

    딕셔너리 상속 추가

--- DataStructure/MyLinearMap.cs
+++ DataStructure/MyLinearMap.cs
@@ -1,12 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace DataStructure
 {
-    public class MyLinearMap
+    public class MyLinearMap<TKey, TValue> : Dictionary<TKey, TValue>
     {
     }
 }
