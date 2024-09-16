1455a9e71195b86610314d8d7c2195a5a57f6c1f
yarogono <70641418+yarogono@users.noreply.github.com>
2024-09-16 22:29:30 +0900

    Key Value 변수 추가

--- DataStructure/MyLinearMap.cs
+++ DataStructure/MyLinearMap.cs
@@ -2,5 +2,8 @@ namespace DataStructure
 {
     public class MyLinearMap<TKey, TValue> : Dictionary<TKey, TValue>
     {
+
+        private KeyCollection? keys;
+        private ValueCollection? values;
     }
 }
