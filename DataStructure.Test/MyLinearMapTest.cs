39d9c64e74a18b47a8d1d36ee64773c7d4fe83ee
yarogono <70641418+yarogono@users.noreply.github.com>
2024-09-17 10:57:23 +0900

    함수 정의 & 테스트 함수 추가

--- DataStructure.Test/MyLinearMapTest.cs
+++ DataStructure.Test/MyLinearMapTest.cs
@@ -1,12 +1,152 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DataStructure.Test
+﻿namespace DataStructure.Test
 {
     public class MyLinearMapTest
     {
+        MyLinearMap<int, string> myLinearMap;
+
+
+        [Fact]
+        public void Add_test()
+        {
+            Assert.Fail("미구현");
+            // Given
+
+
+            // When
+
+
+            // Then
+
+
+        }
+
+        [Fact]
+        public void Remove_test()
+        {
+            Assert.Fail("미구현");
+            // Given
+
+
+            // When
+
+
+            // Then
+
+        }
+
+
+        [Fact]
+        public void Get_test()
+        {
+            Assert.Fail("미구현");
+            // Given
+
+
+            // When
+
+
+            // Then
+
+        }
+
+
+        [Fact]
+        public void Set_test()
+        {
+            Assert.Fail("미구현");
+            // Given
+
+
+            // When
+
+
+            // Then
+
+        }
+
+        [Fact]
+        public void Size_test()
+        {
+            Assert.Fail("미구현");
+            // Given
+
+
+            // When
+
+
+            // Then
+
+        }
+
+        [Fact]
+        public void GetKeys_test()
+        {
+            Assert.Fail("미구현");
+            // Given
+
+
+            // When
+
+
+            // Then
+
+        }
+
+        [Fact]
+        public void GetValues_test()
+        {
+            Assert.Fail("미구현");
+            // Given
+
+
+            // When
+
+
+            // Then
+
+        }
+
+        [Fact]
+        public void ContainsKey_test()
+        {
+            Assert.Fail("미구현");
+            // Given
+
+
+            // When
+
+
+            // Then
+
+        }
+
+        [Fact]
+        public void ContainsValue_test()
+        {
+            Assert.Fail("미구현");
+            // Given
+
+
+            // When
+
+
+            // Then
+
+        }
+
+        [Fact]
+        public void Clear_test()
+        {
+            Assert.Fail("미구현");
+            // Given
+
+            
+            // When
+
+
+            // Then
+
+
+        }
     }
 }
--- DataStructure/MyLinearMap.cs
+++ DataStructure/MyLinearMap.cs
@@ -2,8 +2,68 @@ namespace DataStructure
 {
     public class MyLinearMap<TKey, TValue> : Dictionary<TKey, TValue>
     {
+        private int[]? _buckets;
+        private MyEntry[]? _entries;
 
-        private KeyCollection? keys;
-        private ValueCollection? values;
+        private int _count;
+
+        private KeyCollection? _keys;
+        private ValueCollection? _values;
+
+        public int Count { get { return _count; } }
+
+        public KeyCollection Keys => _keys ??= new KeyCollection(this);
+        public ValueCollection Values => _values ??= new ValueCollection(this);
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                return default;
+            }
+
+            set
+            {
+
+            }
+        }
+
+
+
+        public void Add(TKey key, TValue value)
+        {
+
+        }
+
+        public void Clear()
+        {
+
+        }
+
+        public void Remove(KeyValuePair<TKey, TValue> keyValuePair)
+        {
+
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            return true;
+        }
+
+        public bool ContainsValue(TValue value)
+        {
+            return true;
+        }
+
+
+        public struct MyEntry
+        {
+            public uint hashCode;
+
+            public int next;
+            public TKey key;
+            public TValue value;
+        }
     }
+
 }
